3b04b334bcfc854602754a206ac0032d31293be9
cohyou <cohyou@aurola.net>
2016-07-13 00:01:02 +0900

    add Xcode project

---
+++ Michiru/FeedListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedListViewController.swift
+//  Michiru
+//
+//  Created by OKAZAKI Yasuo on 2016/07/12.
+//  Copyright (c) 2016年 ironoir. All rights reserved.
+//
+
+import UIKit
+
+class FeedListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
