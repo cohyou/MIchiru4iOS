1374f31b55233d5865ef9ed97af88c9ddb5aabbc
OKAZAKI Yasuo <cohyou@ironoir.io>
2016-07-15 21:54:51 +0900

    add view controllers

---
+++ Michiru/JapanController.swift
@@ -0,0 +1,28 @@
+//
+//  JapanController.swift
+//  Michiru
+//
+//  Created by OKAZAKI Yasuo on 2016/07/15.
+//  Copyright © 2016年 ironoir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JapanController: UIViewController {
+    
+    var japanImage: UIImageView!
+    /*
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // firstVCCount += 1
+        /*
+        print("1stViewController's viewDidLoad()is called ----->
+            　　　　　　　　　　　　　　　　　　　　　(Count = \(firstVCCount))")
+*/
+    }
+}
---
+++ Michiru/LoginController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginController.swift
+//  Michiru
+//
+//  Created by OKAZAKI Yasuo on 2016/07/15.
+//  Copyright © 2016年 ironoir. All rights reserved.
+//
+
+import Foundation
